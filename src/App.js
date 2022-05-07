b8a536870b175bd6922f42b5dca4860d2f9a98e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-05-07 04:29:48 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 import {
   Cart,
@@ -9,47 +8,57 @@ import {
   Product,
   ProductList,
   Register,
+  SharedLayout,
   Success,
 } from 'pages/index';
-import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
 import ProtectedRoute from 'utils/ProtectedRoute';
-import Announcement from 'components/Announcement';
+import AuthRoute from 'utils/AuthRoute';
 
-import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Announcement />
-      <Navbar />
-      <ToastContainer />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='products/:category' element={<ProductList />} />
-        <Route path='product/:slug' element={<Product />} />
-        <Route
-          path='register'
-          element={
-            <ProtectedRoute>
-              <Register />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='login'
-          element={
-            <ProtectedRoute>
-              <Login />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='cart' element={<Cart />} />
-        <Route path='success' element={<Success />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='products/:category' element={<ProductList />} />
+          <Route path='product/:slug' element={<Product />} />
+          <Route
+            path='register'
+            element={
+              <ProtectedRoute>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='login'
+            element={
+              <ProtectedRoute>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='cart'
+            element={
+              <AuthRoute>
+                <Cart />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path='success'
+            element={
+              <AuthRoute>
+                <Success />
+              </AuthRoute>
+            }
+          />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }

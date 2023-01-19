24d3fab5ed86316f8f34cad05e002e58bfc2c014
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-19 07:49:50 +0100

    fix: add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import userReducer from 'redux/user/userSlice';
 import cartReducer from 'redux/cart/cartSlice';
 import productReducer from 'redux/products/productSlice';
 
-const store = configureStore({
-  reducer: {
-    products: productReducer,
-    user: userReducer,
-    cart: cartReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  user: userReducer,
+  cart: cartReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };

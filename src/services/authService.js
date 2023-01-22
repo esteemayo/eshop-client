2251d34536544b58af45f7aff46cb6854700ae3f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-22 11:14:07 +0100

    fix: format

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,14 +3,11 @@ import { getFromStorage, tokenKey } from 'utils';
 
 const apiEndpoint = '/auth';
 
-export function login(credentials) {
-  return http.post(`${apiEndpoint}/login`, credentials);
-}
+export const login = (credentials) =>
+  http.post(`${apiEndpoint}/login`, credentials);
 
-export function register(credentials) {
-  return http.post(`${apiEndpoint}/register`, credentials);
-}
+export const register = (credentials) =>
+  http.post(`${apiEndpoint}/register`, credentials);
 
-export function getJWT() {
-  return getFromStorage(tokenKey)?.accessToken;
-}
+export const getJWT = () =>
+  getFromStorage(tokenKey)?.accessToken;

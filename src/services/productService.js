73966134b84979ad121f4ce8b2efc60ad83a6cac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-22 11:17:21 +0100

    fix: format

--- src/services/productService.js
+++ src/services/productService.js
@@ -4,28 +4,17 @@ const apiEndpoint = '/products';
 
 const productUrl = (id) => `${apiEndpoint}/${id}`;
 
-export function getProducts(category) {
-  return http.get(
-    category ? `${apiEndpoint}?category=${category}` : apiEndpoint
-  );
-}
+export const getProducts = (category) =>
+  http.get(category ? `${apiEndpoint}?category=${category}` : apiEndpoint);
 
-export function getProductById(id) {
-  return http.get(productUrl(id));
-}
+export const getProductById = (id) => http.get(productUrl(id));
 
-export function getProductBySlug(slug) {
-  return http.get(`${apiEndpoint}/details/${slug}`);
-}
+export const getProductBySlug = (slug) =>
+  http.get(`${apiEndpoint}/details/${slug}`);
 
-export function createProduct(data) {
-  return http.post(apiEndpoint, data);
-}
+export const createProduct = (data) => http.post(apiEndpoint, data);
 
-export function updateProduct(id, data) {
-  return http.patch(productUrl(id), data);
-}
+export const updateProduct = (id, data) =>
+  http.patch(productUrl(id), data);
 
-export function deleteProduct(id) {
-  return http.delete(productUrl(id));
-}
+export const deleteProduct = (id) => http.delete(productUrl(id));

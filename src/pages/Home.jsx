552646084fbb47314f5c142e066c78454970325f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-20 16:18:24 +0100

    fix: modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import Products from 'components/Products';
 import Categories from 'components/Categories';
+import Slider from 'components/Slider';
 import Newsletter from 'components/Newsletter';
 
 const Home = () => {

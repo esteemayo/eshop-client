7ce46301f462a3878bcc64b93da6936be125235b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-21 07:34:09 +0100

    fix: rename isError state variable to message

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -98,7 +98,7 @@ export const userSlice = createSlice({
       state.isFetching = false;
       state.isSuccess = false;
       state.user = null;
-      state.isError = payload.message;
+      state.message = payload.message;
     },
   },
 });

673b4937aa98bea8c980e3ef1c6bd6efe25f8499
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-21 08:04:04 +0100

    feat: add registerInputs

---
+++ src/formData.js
@@ -0,0 +1,32 @@
+export const registerInputs = [
+  {
+    id: 'name',
+    name: 'name',
+    type: 'text',
+    placeholder: 'Name',
+  },
+  {
+    id: 'username',
+    name: 'username',
+    type: 'text',
+    placeholder: 'Username',
+  },
+  {
+    id: 'email',
+    name: 'email',
+    type: 'email',
+    placeholder: 'you@user.com',
+  },
+  {
+    id: 'password',
+    name: 'password',
+    type: 'password',
+    placeholder: '********',
+  },
+  {
+    id: 'passwordConfirm',
+    name: 'passwordConfirm',
+    type: 'password',
+    placeholder: '********',
+  },
+];

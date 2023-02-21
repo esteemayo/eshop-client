1cbbd21dde2ffa53dbc9621e2efe8f1084d2e11f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-21 14:31:05 +0100

    feat: add icon to file input field

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -140,6 +140,7 @@ const Register = () => {
               />
             )}
             <FormLabel htmlFor='file'>Image</FormLabel>
+            <PublishOutlined className='username__icon' />
           </FormContainer>
           <Agreement>
             By creating an account, I consent to the processing of my personal

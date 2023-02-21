560ae20b71bb4c84a5a606c3daecb67adec418bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-21 07:31:12 +0100

    fix: modify

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -83,7 +83,7 @@ export const userSlice = createSlice({
       state.isFetching = false;
       state.isSuccess = false;
       state.user = null;
-      state.isError = true;
+      state.isError = payload.message;
     },
     [registerUserAsync.pending]: (state) => {
       state.isFetching = true;

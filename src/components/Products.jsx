f2f26c43e4878430fbbe4bf05c50ff2e16e4f78c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-22 16:57:45 +0100

    fix: refactor code

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Product from './Product';
 import { getProducts } from 'services/productService';

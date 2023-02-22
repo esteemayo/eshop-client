5099c8a0647bad7369ef6210f4edec40e642ad45
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-22 19:18:38 +0100

    fix: format code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -193,8 +193,7 @@ const Top = styled.div`
 `;
 
 const TopButton = styled.button`
-  border: ${({ type }) =>
-    type === 'filled' ? 'none' : '2px solid #008080'};
+  border: ${({ type }) => type === 'filled' ? 'none' : '2px solid #008080'};
   background-color: ${(props) =>
     props.type === 'filled' ? '#000' : 'transparent'};
   color: ${(props) => (props.type === 'filled' ? '#fff' : '#008080')};

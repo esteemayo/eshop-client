b9bd2f61e4d2f56ffd1c798f1d5a8e7225a1c83d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-23 02:39:24 +0100

    fix: handle dark/light modes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -256,7 +256,7 @@ const Title = styled.h1`
     content: '';
     width: 13%;
     height: 3px;
-    background-color: #008080;
+    background-color: ${({ theme }) => theme.bgUnderline};
     margin: 0 auto;
 
     ${mobile({ display: 'none' })}

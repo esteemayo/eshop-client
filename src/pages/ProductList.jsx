869b3bf02f992797b3941791886edbf1590af942
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-23 01:10:43 +0100

    fix: modify

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -82,7 +82,6 @@ const Title = styled.h1`
 const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  color: ${({ theme }) => theme.borderFilter};
 `;
 
 const Filter = styled.div`

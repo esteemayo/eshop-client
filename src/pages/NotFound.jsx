8c28db12b975d18167905430be48821c941af752
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-24 01:20:28 +0100

    fix: handle dark/light modes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,6 +12,7 @@ const NotFound = () => {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  background-color: ${({ theme }) => theme.bg};
   display: flex;
   align-items: center;
   justify-content: center;

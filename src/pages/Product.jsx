bb5b2bcf0c1b43d3d45efe6f817f948142d3666a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-24 01:37:41 +0100

    feat: import axios

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,6 +8,7 @@ import { Add, Remove } from '@material-ui/icons';
 import { addProduct } from 'redux/cart/cartSlice';
 import { mobile, tabLand } from 'responsive';
 import { getProductBySlug } from 'services/productService';
+import axios from 'axios';
 
 const Product = () => {
   const dispatch = useDispatch();

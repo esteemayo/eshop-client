9422488f1f4a215ed8a8b3f65abcc3663eec78e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-24 14:24:40 +0100

    fix: modify

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -47,7 +47,7 @@ const IconWrapper = styled.div`
   width: 4rem;
   height: 4rem;
   font-size: 1.5rem;
-  background-color: ${({ theme }) => theme.bgAnnounce};
+  background-color: ${({ theme }) => theme.bgScrollBtn};
   color: ${({ theme }) => theme.textAnnounce};
   box-shadow: rgb(0, 0, 0, 0.25) 0 0.4rem 2rem;
   border-radius: 2rem;

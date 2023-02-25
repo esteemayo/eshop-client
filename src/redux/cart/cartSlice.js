29fe9511cd14e3d986a6e2b5ad776969967dd174
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-25 02:35:13 +0100

    fix: modify

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -14,16 +14,7 @@ export const cartSlice = createSlice({
       state.quantity++;
       state.cart.push(payload);
       state.total += payload.price * payload.quantity;
-    },
-    increment: (state, { payload }) => {
-      const index = state.cart.findIndex((item) => item._id === payload);
-      state.cart[index].quantity++;
-      const subTotal = state.cart[index].price * state.cart[index].quantity;
-      console.log(subTotal)
-      console.log(state.cart[index].price)
-      state.total += subTotal - state.cart[index].price;
-    },
-    reset: (state) => initialState,
+    }, reset: (state) => initialState,
   },
 });
 

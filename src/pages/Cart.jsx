5b0dfb964cd2556da013182fe5c7dba4986de1d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-25 02:35:58 +0100

    fix: modify

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,10 +18,6 @@ const Cart = () => {
   const [stripeToken, setStripeToken] = useState(null);
   const navigate = useNavigate();
 
-  const handleIncrement = (cartId) => {
-    dispatch(increment(cartId));
-  };
-
   const onToken = (token) => {
     setStripeToken(token);
   };

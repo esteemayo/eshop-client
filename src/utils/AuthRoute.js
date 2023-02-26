7f5e0b594e5b93782a1e20165768b58f080d5524
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-26 14:13:34 +0100

    fix: refactor code

--- src/utils/AuthRoute.js
+++ src/utils/AuthRoute.js
@@ -1,6 +1,4 @@
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-
 import LoadingToRedirect from './LoadingToRedirect';
 
 const AuthRoute = ({ children }) => {

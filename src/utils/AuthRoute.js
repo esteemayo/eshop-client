7bacf8d5ba7cd2ae392b551fec53a26a36ae6254
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-26 14:13:01 +0100

    fix: refactor code

--- src/utils/AuthRoute.js
+++ src/utils/AuthRoute.js
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom';
 import LoadingToRedirect from './LoadingToRedirect';
 
 const AuthRoute = ({ children }) => {
-  const location = useLocation();
   const { user } = useSelector((state) => state.user);
 
   return !user ? <Navigate to='/login' replace state={{ from: location }} /> : children;

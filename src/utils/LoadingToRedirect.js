915d1772da78af453e6568797d7615dd32fbbfe0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-26 13:53:16 +0100

    feat: import useNavigate hook

--- src/utils/LoadingToRedirect.js
+++ src/utils/LoadingToRedirect.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const LoadingToRedirect = () => {
+  const navigate = useNavigate();
   const [count, setCount] = useState(5);
 
   return (

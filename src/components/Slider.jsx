ef261ed5dbe4d0f788c1bcff8606e58608cbc980
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-27 15:48:18 +0100

    fix: refactor code

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,7 +24,7 @@ const Slider = () => {
     const lastSlideIndex = sliderItems.length - 1;
 
     const interval = setInterval(() => {
-      setSlideIndex(slideIndex === 2 ? 0 : slideIndex + 1);
+      setSlideIndex(slideIndex === lastSlideIndex ? 0 : slideIndex + 1);
     }, [5000]);
 
     return () => clearInterval(interval);

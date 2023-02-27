181fb985517c613fe0293215d8916106a7d8dbf5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-27 21:50:29 +0100

    fix: refactor

--- src/data.js
+++ src/data.js
@@ -25,7 +25,7 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img: './assets/category-1.webp',
     title: 'SHIRT STYLE!',
     category: 'women',
   },

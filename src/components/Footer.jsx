d1253d35e79deea8d1bdafc81183ac3d63b13817
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-05 02:28:23 +0100

    fix: refactor code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {
-  Instagram,
   LinkedIn,
   MailOutline,
   Phone,

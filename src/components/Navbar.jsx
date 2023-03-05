4170f7963815f36ed97cbb7d70697e4d59843022
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-05 01:35:35 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 import { mobile, small } from '../responsive';
-import { toggle } from 'redux/darkMode/darkModeSlice';
 import { setLogout } from 'redux/user/userSlice';
 
 const Navbar = () => {

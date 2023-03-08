d9431df08fbb7cf3b79b5cc2dd1336e936648974
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-09 00:55:59 +0100

    fix: modify

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Fragment, useEffect, useState } from 'react';
 
 import { stripePayment } from 'services/stripeService';
-import { laptop, mobile, small, smallest, tab } from 'responsive';
 import { getTotals, reset, toggleQuantity } from 'redux/cart/cartSlice';
 
 const Cart = () => {

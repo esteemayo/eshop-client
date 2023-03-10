18ab129ffa1ff9a55229bf1cc05911a9aa2d958b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-10 14:30:20 +0100

    fix: modify

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -187,7 +187,7 @@ const Cart = () => {
               token={onToken}
             >
               <Button>Checkout now</Button>
-            </StripeCheckout> */}
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>

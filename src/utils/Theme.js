bd299109f25f11edd0ec4fe558927cc118de6488
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-10 01:06:13 +0100

    fix: change color value

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -113,5 +113,5 @@ export const lightTheme = {
   boxMode: '0px 0px 4px rgba(82, 76, 76, 0.5)',
   textFooterIcon: 'var(--color-white)',
   bgRight: '#fff8f8',
-  bgRemove: '#ff0000',
+  bgRemove: '#e43131',
 };

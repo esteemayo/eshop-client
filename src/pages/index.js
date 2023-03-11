700c8dba8b63fa434beec412371423d08b133abd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-12 00:47:41 +0100

    feat: import Products page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Login from './Login';
 import NotFound from './NotFound';
 import Product from './Product';
 import ProductList from './ProductList';
+import Products from './Products';
 import Register from './Register';
 import SharedLayout from './SharedLayout';
 import Success from './Success';

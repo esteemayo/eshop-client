0a65944a44fe7c643e1d1725f7de6591823cbb58
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 00:14:50 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -316,7 +316,6 @@ export const products = [
     price: 100,
     inStock: true,
     createdAt: '2023-02-06T15:42:41.179Z',
-    updatedAt: '2023-02-06T15:42:41.179Z',
     slug: 't-shirt',
   },
 ];

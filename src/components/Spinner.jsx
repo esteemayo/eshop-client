abb4bac933d81949a5afe85b127d367d74c855bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 04:09:07 +0100

    fix: handle dark/light mode

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -9,7 +9,7 @@ const Spinner = () => {
 const Container = styled.div`
   width: 6rem;
   height: 6rem;
-  border: 5px solid #019494;
+  border: 5px solid ${({ theme }) => theme.bgSpinner};
   border-radius: 50%;
   border-top-color: #025353;
   animation: spinner 0.6s linear infinite;

18799830669db66bec62406fde41404862e51ecc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 01:10:44 +0100

    feat: handle fetch all products api call

--- src/redux/products/productSlice.js
+++ src/redux/products/productSlice.js
@@ -1,4 +1,17 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getProducts } from 'services/productService';
+
+export const fetchProducts = createAsyncThunk(
+  'products/getProducts',
+  async (category, { rejectWithValue }) => {
+    try {
+      const { data } = await getProducts(category)
+      return data.products;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
 
 const initialState = {
   products: [],

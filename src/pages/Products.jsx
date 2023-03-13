8e2b7f5d3637ff2902be04738b1d5c286c896c5e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 01:00:23 +0100

    fix: modify

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 
 import Product from 'components/Product';
+import { getUnique } from 'utils';
 import { getProducts } from 'services/productService';
 
 const Products = () => {

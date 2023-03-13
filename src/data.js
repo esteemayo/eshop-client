3c0c6b2c5e5494d8973d0f4061a826f0cb62f202
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 03:34:38 +0100

    fix: add url property to the sliderItems array

--- src/data.js
+++ src/data.js
@@ -9,6 +9,7 @@ import {
 export const sliderItems = [
   {
     id: 1,
+    url: '/products',
     img: './assets/hero-1.png',
     title: 'SUMMER SALE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",

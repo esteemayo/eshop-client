6f9d4a308c5667f695c0b08cbf45349dd92d58c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-13 03:48:46 +0100

    feat: add css styles

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -7,7 +7,13 @@ const Spinner = () => {
 };
 
 const Container = styled.div`
-
+  width: 6rem;
+  height: 6rem;
+  border: 5px solid #94a3b8;
+  border-radius: 50%;
+  border-top-color: #645cff;
+  animation: spinner 0.6s linear infinite;
+  margin: 0 auto;
 `;
 
 export default Spinner;

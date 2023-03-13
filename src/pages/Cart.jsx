cc2bf2fb1875886c49ceafdaccd3f2565a33e592
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-14 00:47:01 +0100

    fix: modify

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -144,7 +144,7 @@ const Cart = () => {
               <SummaryItemText>Subtotal</SummaryItemText>
               <SummaryItemPrice>
                 <NumericFormat
-                  value={total}
+                  value={subtotal}
                   displayType={'text'}
                   thousandSeparator={true}
                   prefix={'$'}

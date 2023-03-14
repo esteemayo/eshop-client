2736b546bf7fef30a2faad5178a78fe09496e88c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-14 01:51:41 +0100

    fix: add css properties

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -151,6 +151,10 @@ const Wrapper = styled.div`
 const SpinnerWrapper = styled.div`
   width: 100%;
   height: 100%;
+  position: absolute;
+  top: 95%;
+  left: 50%;
+  transform: translate(-50%,-50%);
 `;
 
 const ImgContainer = styled.div`

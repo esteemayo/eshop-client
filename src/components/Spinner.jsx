6aace9642f993c6a33c5c060449d76ec0b760701
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 00:52:27 +0100

    fix: modify

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,8 +5,12 @@ const Spinner = ({ size }) => {
 };
 
 const Container = styled.div`
-  width: 6rem;
-  height: 6rem;
+  width: ${({ size }) => size === 'sm' && '2rem'};
+  width: ${({ size }) => size === 'md' && '5rem'};
+  width: ${({ size }) => size === 'lg' && '10rem'};
+  height: ${({ size }) => size === 'sm' && '2rem'};
+  height: ${({ size }) => size === 'md' && '5rem'};
+  height: ${({ size }) => size === 'lg' && '10rem'};
   border: 5px solid ${({ theme }) => theme.bgSpinner};
   border-radius: 50%;
   border-top-color: transparent;

26c231f36745b1818ae0ab2ac994d329a02bb8dd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-17 14:19:29 +0100

    fix: modify

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,7 +11,7 @@ const Products = ({ category, filters, sort }) => {
 
   useEffect(() => {
     (async () => {
-        const { token } = await axios.CancelToken.source();
+      const { token } = await axios.CancelToken.source();
       try {
         const { data } = await getProducts(category, token);
         setProducts(data.products);

f835ad28f96e917077f78137c01144e89428b645
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-01-22 04:42:21 +0100

    fix: fix a bug

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -2,4 +2,4 @@ import http from './httpService';
 
 const apiEndpoint = '/orders';
 
-export const createOrder = (data) => http.post(`${apiEndpoint}/orders`, data);
+export const createOrder = (data) => http.post(apiEndpoint, data);
